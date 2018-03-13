d01b4e15298bf07d09c3f664167d6dceb10a1f14
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-03-13 12:16:08 -0500

    Moved to using
    dynamically getting credentials and generating the SQL correctly

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 Lambda function that is notified on DynamoDB table changes and insert/updates a Redshift table. This is designed to be attached to a DynamoDB stream for updates.
 
 # Environment Variables:
-- **REDSHIFT_HOST**
+- **REDSHIFT_CLUSTER_IDENTIFIER** Optional, required if _**REDSHIFT_PASSWORD**_ is not present.
 - **REDSHIFT_DB_NAME**
-- **REDSHIFT_PORT** Optional, defaults to 5439
+- **REDSHIFT_HOST**
+- **REDSHIFT_PASSWORD** Optional, will get temporary credentials from Redshift using _**REDSHIFT_CLUSTER_INDENTIFIER**_ if not provided.
+- **REDSHIFT_PORT** Optional, defaults to _5439_
 - **REDSHIFT_USER**
-- **REDSHIFT_PASSWORD**
-- **DYNAMO_REDSHIFT_ETL** JSON with the following format. Recommend that you minify it. The values in the map "fields" are in JSON Pointer format (RFC6901 - https://tools.ietf.org/html/rfc6901)
+- **DYNAMO_REDSHIFT_ETL** JSON with the following format. Recommend that you minify it. The values in the map _"fields"_ are in JSON Pointer format (RFC6901 - https://tools.ietf.org/html/rfc6901)
 ```
 {
   "<dynamo-table-name>": {
--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -6,32 +6,56 @@ import psycopg2
 import re
 
 from boto3.dynamodb.types import TypeDeserializer
+from datetime import datetime, timedelta
 from jsonpointer import resolve_pointer
 
+base_redshift_connection_string = "host='" + \
+                                  os.environ['REDSHIFT_HOST'] + \
+                                  "' dbname='" + \
+                                  os.environ['REDSHIFT_DB_NAME'] + \
+                                  "' port='" + \
+                                  os.environ.get('REDSHIFT_PORT', '5439') + \
+                                  "'"
 boto3.resource('dynamodb')
+cluster_credentials = {}
+if 'REDSHIFT_PASSWORD' in os.environ:
+    cluster_credentials['DbUser'] = os.environ['REDSHIFT_USER']
+    cluster_credentials['DbPassword'] = os.environ['REDSHIFT_PASSWORD']
+    cluster_credentials['Expiration'] = datetime.max
 deserializer = TypeDeserializer()
 dynamo_redshift_etl = json.loads(os.environ['DYNAMO_REDSHIFT_ETL'])
 dynamo_table_re = re.compile('^arn:aws:dynamodb:[a-z]{2}-[a-z]*-[0-9]:[0-9]*:table/(.+?)/')
-redshift_connection_string = "host='" + \
-                             os.environ['REDSHIFT_HOST'] + \
-                             "' dbname='" + \
-                             os.environ['REDSHIFT_DB_NAME'] + \
-                             "' port='" + \
-                             os.environ.get('REDSHIFT_PORT', '5439') + \
-                             "' user='" + \
-                             os.environ['REDSHIFT_USER'] + \
-                             "' password='" + \
-                             os.environ['REDSHIFT_PASSWORD']
+redshift = boto3.client('redshift')
+
+
+def get_connection_string():
+    if 'Expiration' not in cluster_credentials or cluster_credentials['Expiration'] - timedelta(seconds=30) <= 0:
+        temp_credentials = redshift.get_cluster_credentials(
+            ClusterIdentifier=os.environ['REDSHIFT_CLUSTER_IDENTIFIER'],
+            DbName=os.environ['REDSHIFT_DB_NAME'],
+            DbUser=os.environ['REDSHIFT_USER'],
+            DurationSeconds=3600
+        )
+        cluster_credentials['DbUser'] = temp_credentials['DbUser']
+        cluster_credentials['DbPassword'] = temp_credentials['DbPassword']
+        cluster_credentials['Expiration'] = temp_credentials['Expiration']
+    return base_redshift_connection_string + \
+           " user='" + \
+           cluster_credentials['DbUser'] + \
+           "' password='" + \
+           cluster_credentials['DbPassword'] + \
+           "'"
 
 
 def upsert(logger, redshift_connection, redshift_table, primary_key, fields):
     with redshift_connection.cursor() as cursor:
+        logger.debug('Checking if {} exist. '.format(fields[primary_key]))
         cursor.execute(
             'SELECT * FROM {} WHERE {} = %s'.format(
                 redshift_table,
                 primary_key
             ),
-            (fields[primary_key], )
+            (fields[primary_key],)
         )
         if cursor.fetchone():
             primary_key_value = fields.get(primary_key)
@@ -42,14 +66,11 @@ def upsert(logger, redshift_connection, redshift_table, primary_key, fields):
                     json.dumps(fields)
                 )
             )
-            updates = []
-            for field in fields:
-                if primary_key != field[0]:
-                    updates.append('{0} = {%({0})s}'.format(field[0]))
+            logger.debug(', '.join('{0}=%({0})s'.format(k) for k in fields.keys()))
             cursor.execute(
-                'UPDATE {0} SET {1} WHERE {2} = %{{2})s'.format(
+                'UPDATE {0} SET {1} WHERE {2} = %({2})s'.format(
                     redshift_table,
-                    ', '.join(updates),
+                    ', '.join('{0}=%({0})s'.format(k) for k in fields.keys()),
                     primary_key
                 ),
                 fields
@@ -68,11 +89,10 @@ def upsert(logger, redshift_connection, redshift_table, primary_key, fields):
                 )
             )
             cursor.execute(
-                'INSERT INTO {0}({1}) VALUES ({})'.format(
+                'INSERT INTO {0} ({1}) VALUES ({2})'.format(
                     redshift_table,
                     ', '.join(fields.keys()),
-                    '%({})s, '.join(fields.keys())
-                ),
+                    ', '.join('%({0})s'.format(k) for k in fields.keys())),
                 fields
             )
             logger.info(
@@ -90,8 +110,9 @@ def handler(event, context):
     logger.info('Processing event {}'.format(json.dumps(event)))
 
     if 'Records' in event and len(event['Records']):
-        logger.debug('Connecting to Redshift with connection string: {}'.format(redshift_connection_string))
-        with psycopg2.connect(redshift_connection_string) as redshift_connection:
+        connection_string = get_connection_string()
+        logger.debug('Connecting to Redshift with connection string: {}'.format(connection_string))
+        with psycopg2.connect(connection_string) as redshift_connection:
             logger.info('Connected to redshift')
             for record in filter(lambda x: x['eventName'] == 'INSERT', event['Records']):
                 dynamodb_table = dynamo_table_re.search(record['eventSourceARN']).group(1)
